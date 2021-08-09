259e0b015fbce4bb373adfb353d3a57fa7d69e78
Wei Zhao <zhaowei0524@outlook.com>
2021-08-08 23:25:39 -0400

    改为宽屏

--- docs/conf.py
+++ docs/conf.py
@@ -104,11 +104,15 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 html_sidebars = {
    '**': ['localtoc.html', 'globaltoc.html', 'searchbox.html'],
 }
+
+html_css_files = [
+    'css/my_theme.css',
+]
 
 html_show_sourcelink = False
 
